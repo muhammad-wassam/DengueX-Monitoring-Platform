0762b042248f40359a58ab075dbc8632dd34518d
muhammad-wassam <wassam.b.dev@gmail.com>
2026-07-23 18:43:31 +0500

    Fix Django app configuration for Vercel

--- backend/settings.py
+++ backend/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'chatbot',
+    'backend.apps.BackendConfig',
     'corsheaders',
     'rest_framework.authtoken',
 ]
